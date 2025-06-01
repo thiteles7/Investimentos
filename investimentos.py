4439266f7be91603288bc8b677f7a3768bb0e4cc
thiteles7 <thiagotgnascimento@gmail.com>
2025-06-01 20:23:33 -0300

    Update investimentos.py

--- investimentos.py
+++ investimentos.py
@@ -8,7 +8,7 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 import io
-from fpdf import FPDF
+from fpdf import fpdf
 from streamlit_autorefresh import st_autorefresh
 from datetime import datetime
 from db import initialize_db, get_connection
